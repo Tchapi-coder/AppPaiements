f0983924b59a9f82ef8ff3ee47539498cdfbd808
Ingrid Ines Tchaptchet Nyassi <300140496@monboreal.ca>
2024-10-22 12:46:38 -0400

    creation de la classe Paiement

---
+++ Paiement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        // creation des proprietes 
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+        // creation du constructeur
+        public Paiement(int montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+
+        }
+        // methode afficher 
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"montant: {Montant}, description:{Description}");
+        }
+
+    }
+}
