e33ae3696c9e0872c573213f40b38a0a090ebb57
Ingrid Ines Tchaptchet Nyassi <300140496@monboreal.ca>
2024-10-22 16:06:08 -0400

    ajout de la classe utilisateur

---
+++ Utilisateur.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+        public Utilisateur(string Nom)
+        {
+            Nom = Nom;
+            Paiements = new List<Paiement>();
+        }
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Nom:{Nom}");
+            foreach (Paiement p in Paiements)
+            {
+                p.AfficherDetails();
+
+            }
+
+        }
+        public void AjouterPaiements(Paiement paiement)
+        {
+            Paiements.Add(paiement);
+        }
+    }
+}
+        
